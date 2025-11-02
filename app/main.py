2f723507bcb5f088f6b178d69b3b14bc74212b95
Zanko Yaroslav <arikzanko9@gmail.com>
2025-11-02 09:54:46 +0200

    ms

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 def copy_file(task: str) -> None:
-    paths_files = task.split(" ")
-    if len(paths_files) != 3:
+    command_parts = task.split(" ")
+    if len(command_parts) != 3 or command_parts[1] == command_parts[-1]:
         return
-    if paths_files[0] == "cp":
+    if command_parts[0] == "cp":
         try:
-            with (open(paths_files[1], "r") as f1,
-                  open(paths_files[-1], "w") as f2):
-                for line in f1.readlines():
-                    f2.write(line)
+            with (open(command_parts[1], "r") as source_file,
+                  open(command_parts[-1], "w") as destination_file):
+                for line in source_file.readlines():
+                    destination_file.write(line)
         except FileNotFoundError:
             return
