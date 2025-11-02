a9d6759d182c20f50d4f1194c3b6f0d0d15888de
Zanko Yaroslav <arikzanko9@gmail.com>
2025-11-02 09:33:08 +0200

    ms

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(task: str) -> None:
+    paths_files = task.split(" ")
+    if paths_files[0] == "cp":
+        with open(paths_files[1], "r") as f1, open(paths_files[-1], "w") as f2:
+            for line in f1.readlines():
+                f2.write(line + "\n")
