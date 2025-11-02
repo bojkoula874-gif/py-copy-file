993a6ebd70f045b805a2d792bcd38c7c2f988400
Zanko Yaroslav <arikzanko9@gmail.com>
2025-11-02 09:37:57 +0200

    ms

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(task: str) -> None:
     paths_files = task.split(" ")
     if len(paths_files) != 3:
-        raise Exception("Неправильна команда. Формат: cp <source> <destination>")
+        raise Exception("Неправильна команда. Формат: "
+                        "cp <source> <destination>")
     if paths_files[0] == "cp":
         with open(paths_files[1], "r") as f1, open(paths_files[-1], "w") as f2:
             for line in f1.readlines():
