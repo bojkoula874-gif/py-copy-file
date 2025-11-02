e5d9832acd36d45891da966d33bfd783684f6be5
Zanko Yaroslav <arikzanko9@gmail.com>
2025-11-02 09:42:09 +0200

    ms

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ def copy_file(task: str) -> None:
         return
     if paths_files[0] == "cp":
         try:
-            with open(paths_files[1], "r") as f1, open(paths_files[-1], "w") as f2:
+            with (open(paths_files[1], "r") as f1,
+                  open(paths_files[-1], "w") as f2):
                 for line in f1.readlines():
                     f2.write(line)
         except FileNotFoundError:
             return
-
